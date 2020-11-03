88a6e523d8f1f32a9bf02ba41383551d166ee83c
dyssos29 <o.filippidis@gmail.com>
2020-11-04 00:46:36 +0200

    removed all files from master

--- src/main/java/gr/Pfizer/bootcamp3/team6/restapi/resource/GlucoseStatisticsResource.java
+++
@@ -1,9 +0,0 @@
-package gr.Pfizer.bootcamp3.team6.restapi.resource;
-
-import gr.Pfizer.bootcamp3.team6.restapi.representation.GlucoseStatisticsRepresentation;
-import org.restlet.resource.Get;
-
-public interface GlucoseStatisticsResource {
-    @Get("json")
-    GlucoseStatisticsRepresentation getGlucoseStatistics();
-}
