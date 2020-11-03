f8ebb908158f19a0170ab29dffff67b277637c92
lena8wa,.wqqqqqqqqqqqqqqqgit <e.regkou@gmail.com>
2020-11-04 01:09:32 +0200

    removed .git folder

--- back-end/src/main/java/gr/Pfizer/bootcamp3/team6/restapi/exceptions/DeletedEntityException.java
+++
@@ -1,8 +0,0 @@
-package gr.Pfizer.bootcamp3.team6.restapi.exceptions;
-
-public class DeletedEntityException extends Exception{
-    public DeletedEntityException(String message)
-    {
-        super(message);
-    }
-}
--- back-end/src/main/java/gr/Pfizer/bootcamp3/team6/restapi/model/interfaces/Measurement.java
+++
@@ -1,8 +0,0 @@
-package gr.Pfizer.bootcamp3.team6.restapi.model.interfaces;
-
-import java.util.Date;
-
-public interface Measurement {
-    Date getDateMeasured();
-    double getMeasurementData();
-}
